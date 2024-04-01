0541252a94782e7796793be3c31fa707a958432b
wcywxq <wcywxq@gmail.com>
2024-04-01 21:56:10 +0800

    feat: enable mobile web page enlargement

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' }
+      ]
+    }
+  },
   extends: './packages/ui-pro',
   modules: [
     // '@nuxt/fonts',
